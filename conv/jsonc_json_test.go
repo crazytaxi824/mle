d1d53ef452ecb544a62ed317a8c5eab2cdcb7260
crazytaxi <crazytaxi.git@outlook.com>
2021-06-27 01:21:59 +1000

    update

--- conv/jsonc_json_test.go
+++ conv/jsonc_json_test.go
@@ -1,23 +1,35 @@
 package conv
 
 import (
+	"bytes"
+	"encoding/json"
 	"testing"
 )
 
 const jsoncTest = `{
   // this is comment
-  "s": "abc\" def\" \\\t \n /abc //notComment ~/omg/gg.json", // comment
-  "a": 1,
-  "b": "ok",
-  "c": true,
-  "arr": [
+  "s": "abc\" def\" \\\t \n /abc //notComment { } ~/omg/gg.json", // comment
+  /* haha */ "a": /*dfsd*/ 1,
+  "b */":/* omg */ "ok",
+
+  "c": true, /* abc
+  sfdsfsfs
+  */ "arr": [
     "a", // this is a
     "b",
-    "c"
-  ]
-}
+    "c" /*
+	sfsd
+	*/ // dfhskjf*/
+  ], /* dfhsf
+  fsdfdsf
+  */
+  "sf":{
+	"d":"k",  // haha
+	"e":"o"
+  }  // hahaha
+} // omg
 
-// this is the {comments} after setting.
+// this is the {comment} after setting
 `
 
 func Test_UnmarshalJSONC(t *testing.T) {
@@ -27,4 +39,13 @@ func Test_UnmarshalJSONC(t *testing.T) {
 	}
 
 	t.Log(string(r))
+
+	var buf bytes.Buffer
+	err = json.Indent(&buf, r, "", "  ")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Log(buf.String())
 }
